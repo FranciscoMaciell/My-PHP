4d70911da10731fa2eb9f861b3364ab2c9bb4d88
Francisco Maciel <109698009+FranciscoMaciell@users.noreply.github.com>
2022-07-27 01:55:27 -0300

    att

--- relenbrando/index.php
+++ relenbrando/index.php
@@ -7,35 +7,48 @@
     <title>curso de php</title>
     <style>
         body{
+            background-color: black;
+        }
+
+        main{
             background-color: antiquewhite;
+            width: 700px;
+            height: auto;
+            padding: 20px;
         }
     </style>
 </head>
 <body>
-    <h1>Pegando valores</h1>
-    <form action="index.php" method="get">
-        <p>
-            <label for="ia">valor1</label>
-            <input type="number" name="a" id="ia">
-        </p>
-        <p>
-            <label for="ib">valor2</label>
-            <input type="number" name="b" id="ib">
-        </p>
-        <p>
-            <input type="submit" value="Enviar">
-            <input type="reset" value="Limpar">
-        </p>
-    </form>
+<main>
+    <h1><i>Pegando valores</i></h1>
+        <form action="index.php" method="get">
+            <p>
+                <label for="ia">Valor1</label>
+                <input type="number" name="a" id="ia" step="0.1" require>
+            </p>
+            <p>
+                <label for="ib">Valor2</label>
+                <input type="number" name="b" id="ib" step="0.1" require>
+            </p>
+            <p>
+                <label for="iop">Opção</label>
+                <input type="text" name="op" id="iop" size="4" require>
+            </p>
+            <p>
+                <input type="submit" value="Enviar">
+                <input type="reset" value="Limpar">
+            </p>
+        </form>
+    
+        <?php
+            $val1=$_GET['a'];
+            $val2=$_GET['b'];
+            $tipo=$_GET['op'];
 
-    <?php
-        $valor1=$_GET['a'];
-        $valor2=$_GET['b'];
+            $res=($tipo=='s')?$val1-$val2:$val1*$val2;
 
-        echo "A soma dos valores é: ".($valor1+$valor2);
-        echo "<br>O valor de $valor1 <sup>$valor2</sup> e " . pow($valor1, $valor2);
-        echo "<br>A raiz de $valor1 é " . sqrt($valor1);
-        echo "O valor arredondado de $valor2 é: " . round($valor2);
-    ?>
+            echo "O resultado é $res";
+        ?>
+</main>
 </body>
 </html>

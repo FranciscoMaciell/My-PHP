c5182bd131c3d408af15a32f20b14d20d9b16413
Francisco Maciel <109698009+FranciscoMaciell@users.noreply.github.com>
2022-07-30 23:58:24 -0300

    att

---
+++ relenbrando/matrizes-vetores/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>vetores</title>
+</head>
+<body>
+    <?php
+        /*
+        $c=range(5,20,2);
+        print_r($c);
+        
+
+        // estrutura foreach
+
+        $c=range(5,20,2);
+        foreach($c as $valor){
+            echo $valor. " ";
+        }
+
+        //chaves associativas
+
+        $cad=array("nome"=>"Ana",
+                    "idade"=>23,
+                    "peso"=>78.5);
+
+        foreach($cad as $info=>$info1){
+            echo "O campo $info possui o conteudo $info1<br>";
+        }
+        */
+        
+    ?>
+</body>
+</html>
